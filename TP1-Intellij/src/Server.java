3180a2e5312d1b0bbfc2f08821fe802c659a8467
sbaka <abdelatif4614@gmail.com>
2024-01-12 18:22:43 +0100

    type de message

--- TP1-Intellij/src/Server.java
+++ TP1-Intellij/src/Server.java
@@ -2,41 +2,58 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 class Server  {
     public static void main(String[] args){
-        Utilisateur user1 = new Utilisateur("USER latif","PASS 123");
+        Utilisateur user1 = new Utilisateur("latif","123");
         try{
             int port = 2121;
             ServerSocket server = new ServerSocket(port);
-
             System.out.println("Démarrage du serveur sur 127.0.0.1:"+port+".\r\nAttente d’une connexion...");
+
             Socket client = server.accept();
             OutputStream clientOut = client.getOutputStream();
             InputStream clientInput = client.getInputStream();
             Scanner scanner = new Scanner(clientInput);
+            String username = "",pwd;
+
             //send to client that it is ready
             clientOut.write("220 service ready\n".getBytes());
-            //faire la trace de ce qu'on a reçu
-            String username = scanner.nextLine();
-            System.out.println(username);
-            clientOut.write("331 User name received\n".getBytes());
 
-            String pwd = scanner.nextLine();
-            System.out.println(pwd);
+            while(true){
+                //ce qu'a ecrit le client
+                String input = scanner.nextLine();
 
-            Utilisateur user2 = new Utilisateur(username,pwd);
-            //si le user match
-            if(user1.isSame(user2)){
-                clientOut.write("230 User Logged IN\n".getBytes());
-            }else {
-                clientOut.write("230 User Not Logged \n".getBytes());
+                switch (input.split(" ")[0]){
+
+                    case "USER":
+                        username = input.split(" ")[1];
+                        System.out.println(input);
+                        clientOut.write("331 User name received\r\n".getBytes());
+                        break;
+                    case "PASS":
+                        pwd = input.split(" ")[1];
+                        System.out.println(input);
+                        Utilisateur user2 = new Utilisateur(username,pwd);
+                        if(user1.isSame(user2)){
+                            clientOut.write("230 User Logged IN\r\n".getBytes());
+                        }else {
+                            clientOut.write("430 Invalid username & password\r\n".getBytes());
+                        }
+                        break;
+
+                    case "QUIT":
+                        System.out.println(input);
+                        client.close();
+                        server.close();
+                        System.exit(0);
+                    default:
+                        clientOut.write("500 Command not implemented.\r\n".getBytes());
+                        System.out.println(input);
+                        break;
+                }
             }
-            scanner.nextLine();
-            client.close();
-
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
